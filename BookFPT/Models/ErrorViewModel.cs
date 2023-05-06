157d32b70be03a43653b98abd1d279c99cd240af
Sinh <bi336767@gmail.com>
2023-05-06 15:53:42 +0700

    Add project files.

---
+++ BookFPT/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookFPT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
